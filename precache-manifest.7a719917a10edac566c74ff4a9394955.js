0ee654a309e8701cc3511b7ba2a546ad8688a05a
aaltahhan <57928789+aaltahhan@users.noreply.github.com>
2019-11-20 01:20:55 -0800

    Updates

---
+++ precache-manifest.7a719917a10edac566c74ff4a9394955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b2f4a86b10ae4b66be27233df2e7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f4dee671d90a451f8e",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "d936a6b32e08d63ec83d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "48f4dee671d90a451f8e",
+    "url": "/static/js/2.6d4751f3.chunk.js"
+  },
+  {
+    "revision": "d936a6b32e08d63ec83d",
+    "url": "/static/js/main.a8618ad5.chunk.js"
+  },
+  {
+    "revision": "863a75be61cb442bf56d",
+    "url": "/static/js/runtime-main.fc69315b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
